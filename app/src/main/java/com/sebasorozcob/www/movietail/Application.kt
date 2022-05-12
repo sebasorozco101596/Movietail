b4035774080465aa6fe44852a9c76ec075530342
Juan S Orozco Buitrago <Sebasorozcob@gmail.com>
2022-05-12 15:35:18 -0400

    Initial commit

---
+++ app/src/main/java/com/sebasorozcob/www/movietail/Application.kt
@@ -0,0 +1,7 @@
+package com.sebasorozcob.www.movietail
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class Application : Application()
---
+++ app/src/main/java/com/sebasorozcob/www/movietail/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sebasorozcob.www.movietail.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sebasorozcob.www.movietail.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
