dc803bedbf53ee98d36a2e7cedcc11fb3f283f26
Juan S Orozco Buitrago <Sebasorozcob@gmail.com>
2022-05-16 23:27:06 -0400

    Adding the data for to get the credits of the some movie.

---
+++ domain/src/main/java/com/sebasorozcob/www/domain/use_case/credits/GetCreditsUseCase.kt
@@ -0,0 +1,23 @@
+package com.sebasorozcob.www.domain.use_case.credits
+
+import com.sebasorozcob.www.domain.common.Resource
+import com.sebasorozcob.www.domain.model.Credits
+import com.sebasorozcob.www.domain.repository.CreditsRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import java.io.IOException
+import javax.inject.Inject
+
+class GetCreditsUseCase @Inject constructor(
+    private val repository: CreditsRepository
+){
+    operator fun invoke(movieId: String) : Flow<Resource<Credits>> = flow {
+        try {
+            emit(Resource.Loading())
+            val credits = repository.getCredits(movieId)
+            emit(Resource.Success(credits))
+        } catch (e: IOException) {
+            emit(Resource.Error("Couldn't reach server. Check your internet connection!"))
+        }
+    }
+}
